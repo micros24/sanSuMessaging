54504feaa31a09703de1607f20c7a27dfb6a1830
micros24 <jasperjano.ph@protonmail.com>
2023-06-14 23:16:35 +0800

    Added comment

--- graphql/queries/getMessages.js
+++ graphql/queries/getMessages.js
@@ -10,6 +10,7 @@ module.exports = async (UserModel, from, user) => {
         if(!sender) throw new GraphQLError('Sender is not registered');
 
         const emails = [user.email, sender.email];
+        // Query db of all messages with emails to and from in emails array
         const messages = await MessageModel.findAll({
             where: { 
                 from: { [Op.in]: emails },
