93e6c913be4251f9d999262f5cb9ab57bdc17005
micros24 <jasperjano.ph@protonmail.com>
2023-06-20 18:53:59 +0800

    Added temporary data for testing

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -4,9 +4,20 @@ import { useAuthDispatch } from "../context/auth";
 import Row from "react-bootstrap/Row";
 import { Col } from "react-bootstrap";
 
-const GET_FRIENDS = gql`
-  query getFriends {
-    getFriends {
+// const GET_FRIENDS = gql`
+//   query getFriends {
+//     getFriends {
+//       email
+//       firstName
+//       lastName
+//       profilePicture
+//     }
+//   }
+// `;
+
+const GET_USERS = gql`
+  query getUsers {
+    getUsers {
       email
       firstName
       lastName
@@ -19,7 +30,7 @@ export default function ChatWindow() {
   const dispatch = useAuthDispatch();
   const [errors, setErrors] = useState(Object);
 
-  const { loading, data, error } = useQuery(GET_FRIENDS);
+  const { loading, data, error } = useQuery(GET_USERS);
   //TODO: add logic to find who are your friends
 
   if (error) {
@@ -27,21 +38,34 @@ export default function ChatWindow() {
   }
 
   if (data) {
-    console.log(data);
+    //console.log(data);
   }
-
   let friendsMarkup;
   if (!data || loading) {
     friendsMarkup = <p>Loading...</p>;
-  } else if (data.getFriends.length === 0) {
+  } else if (data.getUsers.length === 0) {
     friendsMarkup = <p>Add some friends to start chatting :)</p>;
-  } else if (data.getFriends.length > 0) {
-    friendsMarkup = data.getFriends.map((user) => (
+  } else if (data.getUsers.length > 0) {
+    friendsMarkup = data.getUsers.map((user) => (
       <div key={user.email}>
         <p>{user.email}</p>
       </div>
     ));
   }
+
+  // let friendsMarkup;
+  // if (!data || loading) {
+  //   friendsMarkup = <p>Loading...</p>;
+  // } else if (data.getFriends.length === 0) {
+  //   friendsMarkup = <p>Add some friends to start chatting :)</p>;
+  // } else if (data.getFriends.length > 0) {
+  //   friendsMarkup = data.getFriends.map((user) => (
+  //     <div key={user.email}>
+  //       <p>{user.email}</p>
+  //     </div>
+  //   ));
+  // }
+
   // const [getFriends, { loading }] = useQuery(GET_FRIENDS, {
   //   onError: (error) => setErrors(error.graphQLErrors[0].extensions.errors),
   //   onCompleted(data) {},
