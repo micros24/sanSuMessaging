079ea4391cd697d3d3fe7762e7ec9a9e408d4a64
micros24 <jasperjano.ph@protonmail.com>
2023-06-21 23:13:46 +0800

    Rename remaining notifications to friendRequests

--- client/src/components/modals/FriendRequestsModal.tsx
+++ client/src/components/modals/FriendRequestsModal.tsx
@@ -3,7 +3,7 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { useNavigate, Link } from "react-router-dom";
 
-export default function NotificationsModal() {
+export default function FriendRequestsModal() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -12,7 +12,7 @@ export default function NotificationsModal() {
   return (
     <>
       <Button
-        id="btnShowNotificationsModal"
+        id="btnShowFriendRequestsModal"
         variant="primary"
         onClick={handleShow}
         hidden
@@ -22,17 +22,7 @@ export default function NotificationsModal() {
           <Modal.Title>Friend Requests</Modal.Title>
         </Modal.Header>
         <Modal.Body className="text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-person-fill-add"
-            viewBox="0 0 16 16"
-          >
-            <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            <path d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Z" />
-          </svg>
+          <p>tester body</p>
         </Modal.Body>
         <Modal.Footer>
           <div>
