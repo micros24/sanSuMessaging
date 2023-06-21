7b7d8fa2045707f5c795145d578267fc398c9615
micros24 <jasperjano.ph@protonmail.com>
2023-06-22 00:54:15 +0800

    FriendRequestsModal real-time functional

--- client/src/components/modals/FriendRequestsModal.tsx
+++ client/src/components/modals/FriendRequestsModal.tsx
@@ -22,7 +22,6 @@ interface Props {
 
 let users;
 export default function NotificationsModal({ isNewLogin }: Props) {
-  const [friendRequests, setFriendRequests] = useState();
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -33,7 +32,7 @@ export default function NotificationsModal({ isNewLogin }: Props) {
   const { refetch } = useQuery(FRIEND_REQUESTS, {
     pollInterval: 5000, // Poll every 5 seconds
     onCompleted(data) {
-      setFriendRequests(data.getFriendRequests);
+      users = data.getFriendRequests;
     },
   });
   if (isNewLogin === true) {
@@ -54,37 +53,37 @@ export default function NotificationsModal({ isNewLogin }: Props) {
           <Modal.Title>Friend Requests</Modal.Title>
         </Modal.Header>
         <Modal.Body className="text-center">
-          {/* TODO: */}
-          {users ? (
-            users.map((user) => (
-              <ListGroup.Item key={user.email}>
-                <div className="d-flex justify-content-between align-items-center">
-                  {user.profilePicture} {user.firstName} {user.lastName} (
-                  {user.email})
-                  <Button
-                    variant="primary"
-                    type="submit"
-                    onClick={(e) => {
-                      e.currentTarget.classList.add("disabled");
-                      e.currentTarget.classList.replace(
-                        "btn-primary",
-                        "btn-success"
-                      );
-                      e.currentTarget.innerText = "Request sent!";
-                      setFormData({ ...formData, recipient: user.email });
-                    }}
-                  >
-                    Accept request
-                  </Button>
-                </div>
-              </ListGroup.Item>
-            ))
-          ) : (
-            <span className="text-center">
-              People that requested you as a friend will appear here.
-            </span>
-          )}
-          {/* TODO: */}
+          <ListGroup variant="flush">
+            {users ? (
+              users.map((user) => (
+                <ListGroup.Item key={user.sender}>
+                  <div className="d-flex justify-content-between align-items-center">
+                    {user.senderProfilePicture} {user.senderFirstName}{" "}
+                    {user.senderLastName} ({user.sender})
+                    <Button
+                      variant="primary"
+                      type="submit"
+                      onClick={(e) => {
+                        e.currentTarget.classList.add("disabled");
+                        e.currentTarget.classList.replace(
+                          "btn-primary",
+                          "btn-success"
+                        );
+                        e.currentTarget.innerText = "Request sent!";
+                        setFormData({ ...formData, recipient: user.sender });
+                      }}
+                    >
+                      Accept request
+                    </Button>
+                  </div>
+                </ListGroup.Item>
+              ))
+            ) : (
+              <span className="text-center">
+                People that requested you as a friend will appear here.
+              </span>
+            )}
+          </ListGroup>
         </Modal.Body>
         <Modal.Footer>
           <div>
