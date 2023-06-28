077fa4697d6d98d5cfc1a5ca0082d37d495fc0c8
micro24 <jasperjano.ph@protonmail.com>
2023-06-29 00:46:15 +0800

    Added missing files

--- src/graphql/queries/getUsers.js
+++ src/graphql/queries/getUsers.js
@@ -1,6 +1,7 @@
 const { GraphQLError } = require ('graphql');
 const { Op } = require("sequelize");
 const getSentFriendRequests = require('./getSentFriendRequests');
+const getFriends = require("./getFriends");
 const validateEmail = (email) => {
     return String(email)
       .toLowerCase()
@@ -21,7 +22,7 @@ module.exports = async (UserModel, name, user) => {
                 throw "The name cannot be empty!";
             }
 
-            let users;
+            let users = [];
             if(validateEmail(name)) {
                 // Input is an email address
                 users = await UserModel.findAll({
@@ -69,6 +70,19 @@ module.exports = async (UserModel, name, user) => {
             // User is not registered
             if(!users[0]) throw "A person with that name/email is not registered.";
 
+            // remove all of my friends from the lsit (search for a friend)
+            const friends = await getFriends(UserModel, user);
+            let tempUsers;
+            // friend == UsersModel
+            users.forEach(person => {
+                friends.forEach((friend, index) => {
+                    if(friend.email === person.email) {
+                        delete users[index];
+                    }
+                });
+            });
+            
+
             // Get all friend requests I have sent
             const sentFriendRequests = await getSentFriendRequests(user);
             let friendRequestSentChecker = [];
@@ -84,7 +98,8 @@ module.exports = async (UserModel, name, user) => {
                 })
             );
                 
-            // users = the list of person with the name i am searching
+            // users = the list of person with the name i am searching,
+            // filtered to remove my friends
             // sentFriendRequests = all friend requests that I have sent
             users.forEach(person => {
                 if(sentFriendRequests[0]) {
